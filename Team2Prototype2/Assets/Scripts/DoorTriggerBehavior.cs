0e1aa2fd709cebfb0d9192a342794c4163d9b4ed
Karkaranos <karkaranosyt@gmail.com>
2024-02-25 12:26:45 -0600

    Bug Fixing
    -1st and 3rd doors will no longer move together
    -Level can be reset
    -Application can be quit

--- Team2Prototype2/Assets/Scripts/DoorTriggerBehavior.cs
+++ Team2Prototype2/Assets/Scripts/DoorTriggerBehavior.cs
@@ -9,6 +9,8 @@ using UnityEngine;
 
 public class DoorTriggerBehavior : MonoBehaviour
 {
+    [Tooltip("If this door works opposite another, the door opposite it")]
+    [SerializeField] private GameObject _linkedDoor;
     /// <summary>
     /// Closes the door on contact with an object
     /// </summary>
@@ -16,5 +18,9 @@ public class DoorTriggerBehavior : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         gameObject.GetComponentInParent<DoorBehavior>().CloseDoor();
+        if(_linkedDoor!=null)
+        {
+            _linkedDoor.GetComponent<DoorBehavior>().OpenDoor();
+        }
     }
 }
